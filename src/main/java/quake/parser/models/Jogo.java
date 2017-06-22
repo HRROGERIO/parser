90bd6197ebe0f4a5d34b2848e2ef17d1aa22891a
HRROGERIO <HRROGERIO@YAHOO.COM.BR>
2017-06-21 22:58:30 -0300

    primeiro commit inicio projeto

---
+++ src/main/java/quake/parser/models/Jogo.java
@@ -0,0 +1,49 @@
+package quake.parser.models;
+
+import java.util.Map;
+
+public class Jogo {
+
+	private String name;
+	private Integer totalKills;
+	private Map<String, Integer> killsForPlayers;
+	private Map<String, Integer> meansOfDeath;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+	public Integer getTotalKills() {
+		return totalKills;
+	}
+
+	public void setTotalKills( Integer totalKills ) {
+		this.totalKills = totalKills;
+	}
+
+	public Map<String, Integer> getKillsForPlayers() {
+		return killsForPlayers;
+	}
+
+	public void setKillsForPlayers( Map<String, Integer> killsForPlayers ) {
+		this.killsForPlayers = killsForPlayers;
+	}
+
+	public Map<String, Integer> getMeansOfDeath() {
+		return meansOfDeath;
+	}
+
+	public void setMeansOfDeath( Map<String, Integer> meansOfDeath ) {
+		this.meansOfDeath = meansOfDeath;
+	}
+
+	@Override
+	public String toString() {
+		return "Game [name=" + name + ", totalKills=" + totalKills + ", killsForPlayers=" + killsForPlayers + "]";
+	}
+
+}
